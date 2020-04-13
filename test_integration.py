c6440e2f5c2907fc4d983d5fc002762ae99d45be
Brian Jubelirer <brian2386@gmail.com>
2020-04-13 13:32:59 -0400

    add delete unit tests

--- test_integration.py
+++ test_integration.py
@@ -1,39 +1,43 @@
 from jse import *
 import pytest
 
+
 def test_basic_edit():
-    json_obj = {'a':'b'}
+    json_obj = {'a': 'b'}
     query = 'a'
     value = 'x'
-    func = edit_func    
-    obj = operate_on_key(json_obj,query,value,func)
-    assert obj == {'a':'x'}
+    func = edit_func
+    obj = operate_on_key(json_obj, query, value, func)
+    assert obj == {'a': 'x'}
+
 
 def test_basic_add():
-    json_obj = {'a':'b'}
+    json_obj = {'a': 'b'}
     query = 'c'
     value = 'x'
-    func = add_func    
-    obj = operate_on_key(json_obj,query,value,func)
-    assert obj == {'a':'b','c':'x'}
+    func = add_func
+    obj = operate_on_key(json_obj, query, value, func)
+    assert obj == {'a': 'b', 'c': 'x'}
+
 
 def test_list_add_auto_index():
     listname = "mylist"
-    json_obj = {listname:[]}
+    json_obj = {listname: []}
     value = 'x'
     func = add_func
-    obj = operate_on_key(json_obj,listname,value,func)
-    assert obj == {listname:[value]}
+    obj = operate_on_key(json_obj, listname, value, func)
+    assert obj == {listname: [value]}
 
 
 def test_list_add_manual_index():
     listname = "mylist"
-    json_obj = {listname:[]}
+    json_obj = {listname: []}
     query = listname + ".0"
     value = 'x'
     func = add_func
-    obj = operate_on_key(json_obj,query,value,func)
-    assert obj == {listname:[value]}
+    obj = operate_on_key(json_obj, query, value, func)
+    assert obj == {listname: [value]}
+
 
 def test_obj_add():
     json_obj = {
@@ -42,12 +46,13 @@ def test_obj_add():
     query = "my_obj.name"
     value = "waldo"
     func = add_func
-    obj = operate_on_key(json_obj,query,value,func)
+    obj = operate_on_key(json_obj, query, value, func)
     assert obj == {
-        "my_obj" : {
-            "name":"waldo"
+        "my_obj": {
+            "name": "waldo"
         }
     }
+
 
 def test_obj_add_example():
     json_obj = {}
@@ -55,17 +60,86 @@ def test_obj_add_example():
     value = "[{score:32.5,user:bob,metadata:{ip:192.168.1.102,client:firefox}},{}]"
     value = typed_value(value)
     func = add_func
-    obj = operate_on_key(json_obj,query,value,func)
-   
+    obj = operate_on_key(json_obj, query, value, func)
+
     assert obj == {
         "highscore": [
             {
-                "score":32.5,
+                "score": 32.5,
                 "user": "bob",
                 "metadata": {
-                    "ip":"192.168.1.102",
-                    "client":"firefox"
+                    "ip": "192.168.1.102",
+                    "client": "firefox"
                 }
-            },{}
+            }, {}
         ]
     }
+
+
+def test_delete():
+    json_obj = {
+        "users": [
+            {"id": 1, "name": "mike"},
+            {"id": 2, "name": "jon"}
+        ],
+        "map": {
+            "a": 3,
+            "x": 9
+        }
+    }
+
+    obj = operate_on_key(json_obj, "users.1", None, delete_func)
+    assert obj == {
+        "users": [
+            {"id": 1, "name": "mike"}
+        ],
+        "map": {
+            "a": 3,
+            "x": 9
+        }
+    }
+    obj = operate_on_key(obj, "map", None, delete_func)
+    assert obj == {
+        "users": [
+            {"id": 1, "name": "mike"}
+        ]
+    }
+
+def test_list_regex_delete():
+    json_obj = {
+        "users": [
+            {"id": 1, "name": "mike"},
+            {"id":7,"name":"paul"},
+            {"id": 2, "name": "jon"}
+        ],
+        "map": {
+            "a": 3,
+            "x": 9
+        }
+    }
+    # delete last entry in list
+    obj = operate_on_key(json_obj,"users.$",None,delete_func)
+    assert obj == {
+        "users": [
+            {"id": 1, "name": "mike"},
+            {"id":7,"name":"paul"}
+        ],
+        "map": {
+            "a": 3,
+            "x": 9
+        }
+    }
+    obj = operate_on_key(json_obj,"users.^",None,delete_func)
+    assert obj == {
+        "users": [
+            {"id":7,"name":"paul"},
+            {"id": 2, "name": "jon"}
+        ],
+        "map": {
+            "a": 3,
+            "x": 9
+        }
+    }
+
+
+# todo - test error handling and seperate exit from error states
