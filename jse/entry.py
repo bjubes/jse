27b075b99a27da8fcc030bcb4e40d867e46c05d8
Brian Jubelirer <brian2386@gmail.com>
2020-04-15 23:44:47 -0400

    using click

--- jse/entry.py
+++ jse/entry.py
@@ -3,15 +3,32 @@ import json
 import sys
 import click
 
-@click.group()
+VERSION = "0.0.5"
+
+
+class AliasedGroup(click.Group):
+    def get_command(self, ctx, cmd_name):
+        rv = click.Group.get_command(self, ctx, cmd_name)
+        if rv is not None:
+            return rv
+        matches = [x for x in self.list_commands(ctx) if x.startswith(cmd_name)]
+        if not matches:
+            return None
+        elif len(matches) == 1:
+            return click.Group.get_command(self, ctx, matches[0])
+        ctx.fail('Too many matches: {}'.format(', '.join(sorted(matches))))
+
+@click.group(cls=AliasedGroup)
 @click.argument('file')
 @click.pass_context
+@click.version_option(version=VERSION)
 def jse(ctx,file):
     ctx.obj = file
 
-@jse.command()
+@jse.command(short_help='add a new element or append to a list')
 @click.argument('query')
 @click.argument('value', nargs=-1)
+@click.option('-p','--preview', is_flag=True,help="preview change without writing to file")
 @click.pass_obj
 def add(file,query,value,preview):
     value = parse_value_from_bash(value)
@@ -23,13 +40,13 @@ def add(file,query,value,preview):
         return
     save_json(file,json) 
 
-
-@jse.command()
+@jse.command(short_help='change an existing key to a new value')
 @click.argument('query')
 @click.argument('value', nargs=-1)
+@click.option('-p','--preview', is_flag=True,help="preview change without writing to file")
 @click.pass_obj
 def edit(file,query,value,preview):
-    value = parse_value_from_bash(query)
+    value = parse_value_from_bash(value)
     json = load_json(file)
     obj,key = query_object(json,query)
     edit_func(obj,key,value)
@@ -38,7 +55,7 @@ def edit(file,query,value,preview):
         return
     save_json(file,json) 
 
-@jse.command()
+@jse.command(short_help='delete an element')
 @click.argument('query', nargs=-1)
 @click.pass_obj
 def delete(file,query):
@@ -47,6 +64,9 @@ def delete(file,query):
         obj,key = query_object(json,q)
         delete_func(obj,key)
     save_json(file,json) 
+
+
+
 
 # values can be corrupted into a list of arguments by bash brace expansion
 # this function handles converting value that was passed via the cmdline
