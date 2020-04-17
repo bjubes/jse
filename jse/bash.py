c909c4d607fd08cc077a873bc609c79a45151200
Brian Jubelirer <brian2386@gmail.com>
2020-04-17 19:18:55 -0400

    Improve bash parsing (#7)

--- jse/bash.py
+++ jse/bash.py
@@ -1,3 +1,4 @@
+import math
 
 from .editor import *
 
@@ -9,9 +10,9 @@ def parse_value_from_bash(value):
         return parse_value(value[0])
     try:
         if all(arg[0] == "[" and arg[-1] == "]" for arg in value):
-            #the user has passed [value] and bash has messed with it.
+            # the user has passed [value] and bash has messed with it.
             print(value)
-            value = fix_bash_brackets(value)
+            value = parse_bash_brackets_value(value)
         else:
             # the user passed {value} and bash has messed with it
             value = ",".join(value)          
@@ -21,51 +22,84 @@ def parse_value_from_bash(value):
         raise EditorError("too many arguments were passed or bash has preprocessed and mangled your input. Try putting VALUE in quotes.") from err
     return value
 
-def fix_bash_brackets(elements):
-    cnt = len(elements[0].split(','))
-    obj_list = []
-    for i in range(cnt):
-        args = []
-        for elem in elements:
-            args.append(elem[1:-1].split(',',1)[i])
-        # a hack to see when to parse nested vs semetric elements in a list
-        key = elements[0].split(':',1)[0][1:]
-        if key in elements[1] and (key in elements[len(elements)-2] or key in elements[2]):
-            #this is the dot product pattern of semetrical elements
-            return parse_brackets_semetric(elements)
-        else:
-            #nested parsing
-            obj = parse_colons(args)
-            obj_list.append(obj)
-    return obj_list
+# takes in elements from bash and returns the value as a parsed python object
+# performs a series of checks and passes work to other functions
+def parse_bash_brackets_value(elements):
+    if any(x[:x.find(',')].count(':')>1 for x in elements):
+        # there are subkeys in this object
+        return parse_nested_object_list(elements)
+    elif all(x.count(',') == 0 for x in elements):
+        # no commas in any element means there is only one element in the list
+        return parse_split_object(elements)
+    else:
+        return parse_brackets_semetric(elements)
+
+def parse_split_object(attrs):
+    # the object has been split, so {a:1,b:2} is now
+    # [a:1]     [b:2]
+    obj = {}
+    for a in attrs:
+        a = a[1:-1]
+        k,v = a.split(":",1)
+        obj[k] = parse_value(v)
+    return [obj]
+
+# given a bash expanded list, determine how many objects were passed and how many
+# elements each object had. assumes all elements were of the same attr length
+def get_obj_attr_count(elements):
+    num_obj = elements[0].count(",") + 1
+    #length = num_attrs^ num_obj
+    num_attrs = math.log(len(elements)) / math.log(num_obj)
+    return num_obj,int(round(num_attrs))
+
 
 def parse_brackets_semetric(elements):
-    # hardcoded indexes where uniform vectors fall. has to do with
-    # where the identity matrix 1s are
-    indexes = []
-    length = len(elements)
-    if length == 4:
-        indexes = [0,3]
-    if length == 8:
-        indexes = [0,7]
-    if length == 9:
-        indexes = [0,4,8]
-    if length == 27:
-        indexes = [0,13,26]
-    
-    selected_elems = [elements[n] for n in indexes]
-    elems = [parse_value(s.replace('[', '[{').replace(']','}]').replace(',','},{')) for s in selected_elems]
-    combine = list(zip(*elems))
-    obj_list = []
-    for tup in combine:
-        obj = {}
-        for o in tup:
-            obj[list(o.keys())[0]] = list(o.values())[0]
-        obj_list.append(obj)
-    return obj_list
+    n_obj,n_attr = get_obj_attr_count(elements)
+    tree = []
+    for e in elements:
+        subobjs = e[1:-1].split(',')
+        elem = []
+        for s in subobjs:
+            kvp = s.split(':')
+            elem.append(kvp)
+        tree.append(elem)
+    print(tree)
 
-def parse_colons(elements):
-    print(elements)
+    fixed_input = []
+    for obj_i in range(n_obj):
+        obj = {}
+        for atr_i in range(n_attr):
+            # rem_obj is how many objects 
+            tree_index = atr_i*n_attr**((n_obj-1-obj_i))
+            k = tree[tree_index][obj_i][0] 
+            v = tree[tree_index][obj_i][1]
+            obj[k] = parse_value(v)
+        fixed_input.append(obj)
+    return fixed_input
+
+def parse_nested_object_list(elements):
+    fixed_list = []
+    elems = []
+    for e in elements:
+        elem = e
+        if e[0] == '[':
+            elem = elem[1:]
+        if e[-1] == ']' or e[-1] ==',':
+            elem = elem[:-1]
+        elems.append(elem)
+    print(elems)
+    ptr = 0
+    key = None
+    for i,e in enumerate(elems):
+        if key == None:
+            key = e.split(':',1)[0]
+        elif e.split(':',1)[0] == key:
+            fixed_list.append(parse_nested_object(elems[ptr:i]))
+            ptr = i
+    fixed_list.append(parse_nested_object(elems[ptr:]))
+    return fixed_list
+
+def parse_nested_object(elements):
     obj = {}
     for elem in elements:
         if ':' in elem:
@@ -76,5 +110,5 @@ def parse_colons(elements):
                 obj[k] = [obj[k],parse_value(v)]
     for k in obj:
         if type(obj[k]) == list:
-           obj[k] = parse_colons(obj[k])
+           obj[k] = parse_nested_object(obj[k])
     return obj
--- tests/test_bash_expansion.py
+++ tests/test_bash_expansion.py
@@ -7,7 +7,7 @@ import pytest
 def test_nested_object():
     #input:  [{score:32.5,user:bob,metadata:{ip:192.168.1.102,client:firefox}}]
     corrupted_input = ['[score:32.5]', '[user:bob]', '[metadata:ip:192.168.1.102]', '[metadata:client:firefox]']
-    value = fix_bash_brackets(corrupted_input)
+    value = parse_bash_brackets_value(corrupted_input)
     assert value == [{'user':'bob',
                      'score': 32.5,
                      'metadata':{
@@ -15,11 +15,29 @@ def test_nested_object():
                          'client':'firefox'
                      }}]
 
+def test_nested_object_list():
+    #input: [{score:32.5,user:bob,metadata:{ip:192.168.1.102,client:firefox}}, {score:21.0,user:steve,metadata:{ip:192.168.1.147,client:chrome}}]
+    corrupted_input = ['[score:32.5,', '[user:bob,', '[metadata:ip:192.168.1.102,', '[metadata:client:firefox,', 'score:21.0]', 'user:steve]', 'metadata:ip:192.168.1.147]', 'metadata:client:chrome]']
+    value = parse_bash_brackets_value(corrupted_input)
+    assert value == [{'user':'bob',
+                     'score': 32.5,
+                     'metadata':{
+                         'ip':'192.168.1.102',
+                         'client':'firefox'
+                     }},
+                     {'user':'steve',
+                     'score': 21.0,
+                     'metadata':{
+                         'ip':'192.168.1.147',
+                         'client':'chrome'
+                     }}]
+
+
 def test_same_keys_object_list():
     # 3x3
     #input: [{name:bob,id:1},{name:steve,id:3}, {name:ted,id:5}]
     corrupted_input = ['[name:bob,name:steve,name:ted]', '[name:bob,name:steve,id:5]', '[name:bob,name:steve,g:6]', '[name:bob,id:3,name:ted]', '[name:bob,id:3,id:5]', '[name:bob,id:3,g:6]', '[name:bob,g:4,name:ted]', '[name:bob,g:4,id:5]', '[name:bob,g:4,g:6]', '[id:1,name:steve,name:ted]', '[id:1,name:steve,id:5]', '[id:1,name:steve,g:6]', '[id:1,id:3,name:ted]', '[id:1,id:3,id:5]', '[id:1,id:3,g:6]', '[id:1,g:4,name:ted]', '[id:1,g:4,id:5]', '[id:1,g:4,g:6]', '[g:2,name:steve,name:ted]', '[g:2,name:steve,id:5]', '[g:2,name:steve,g:6]', '[g:2,id:3,name:ted]', '[g:2,id:3,id:5]', '[g:2,id:3,g:6]', '[g:2,g:4,name:ted]', '[g:2,g:4,id:5]', '[g:2,g:4,g:6]']
-    value = fix_bash_brackets(corrupted_input)
+    value = parse_bash_brackets_value(corrupted_input)
     assert value == [
         {'name':'bob','id':1,'g':2},
         {'name':'steve','id':3, 'g':4},
@@ -28,7 +46,7 @@ def test_same_keys_object_list():
     # 3x2
     #input: [{name:bob,id:1,x:3},{name:steve,id:3,x:5}]
     corrupted_input = ['[name:bob,name:steve]', '[name:bob,id:3]', '[name:bob,x:5]', '[id:1,name:steve]', '[id:1,id:3]', '[id:1,x:5]', '[x:3,name:steve]', '[x:3,id:3]', '[x:3,x:5]']
-    value = fix_bash_brackets(corrupted_input)
+    value = parse_bash_brackets_value(corrupted_input)
     assert value == [
         {'name':'bob','id':1,'x':3},
         {'name':'steve','id':3, 'x':5},
@@ -36,7 +54,7 @@ def test_same_keys_object_list():
     # 2x2
     #input: [{name:bob,id:1},{name:steve,id:3}]
     corrupted_input = ['[name:bob,name:steve]', '[name:bob,id:3]', '[id:1,name:steve]', '[id:1,id:3]']
-    value = fix_bash_brackets(corrupted_input)
+    value = parse_bash_brackets_value(corrupted_input)
     assert value == [
         {'name':'bob','id':1,},
         {'name':'steve','id':3}
@@ -44,34 +62,28 @@ def test_same_keys_object_list():
 
     #input:  [{name:bob,id:1},{name:steve,id:3},{name:ted,id:5}]
     corrupted_input = ['[name:bob,name:steve,name:ted]', '[name:bob,name:steve,id:5]', '[name:bob,id:3,name:ted]', '[name:bob,id:3,id:5]', '[id:1,name:steve,name:ted]', '[id:1,name:steve,id:5]', '[id:1,id:3,name:ted]', '[id:1,id:3,id:5]']
-    value = fix_bash_brackets(corrupted_input)
+    value = parse_bash_brackets_value(corrupted_input)
     assert value == [
         {'name':'bob','id':1,},
         {'name':'steve','id':3},
         {'name':'ted','id':5}
     ]
 
-# failling currently
-# 
-# need to implement a consistent way to deal with
-# any length list with objects with any number of parameters
-#
-# def test_unique_object_list():
-#     # 2x2
-#     #input:  [{a:1,b:2},{c:3,d:4}]
-#     corrupted_input = ['[a:1,c:3]', '[a:1,d:4]', '[b:2,c:3]', '[b:2,d:4]']
-#     value = fix_bash_brackets(corrupted_input)
-#     assert value == [
-#         {"a":1,"b":2},
-#         {"b":3,"c":4}
-#     ]
-
+def test_unique_object_list_2x2():
+    # 2x2
+    #input:  [{a:1,b:2},{c:3,d:4}]
+    corrupted_input = ['[a:1,c:3]', '[a:1,d:4]', '[b:2,c:3]', '[b:2,d:4]']
+    value = parse_bash_brackets_value(corrupted_input)
+    assert value == [
+        {"a":1,"b":2},
+        {"c":3,"d":4}
+    ]
 
 def test_simple_obj_list():
     # 3x1
     #input: [{a:1,b:2,c:3}]
     corrupted_input = ['[a:1]', '[b:2]', '[c:3]']
-    value = fix_bash_brackets(corrupted_input)
+    value = parse_bash_brackets_value(corrupted_input)
     assert value == [
         {"a":1,"b":2,"c":3}
     ]
@@ -84,4 +96,24 @@ def test_simple_obj_list():
         {"a":1},
         {"b":2},
         {"c":3}
+    ]
+
+def test_unique_object_list_3x2():
+    #input: [{a:1,b:2,c:3},{d:4,e:5,f:6}]
+    corrupted_input = ['[a:1,d:4]', '[a:1,e:5]', '[a:1,f:6]', '[b:2,d:4]', '[b:2,e:5]', '[b:2,f:6]', '[c:3,d:4]', '[c:3,e:5]', '[c:3,f:6]']
+    value = parse_bash_brackets_value(corrupted_input)
+    assert value == [
+       {"a":1,"b":2,"c":3},
+       {"d":4,"e":5,"f":6}
+    ]
+
+
+def test_unique_object_list_2x3():
+    #input: [{a:1,b:2},{c:3,d:4},{e:5,f:6}]
+    corrupted_input = ['[a:1,c:3,e:5]', '[a:1,c:3,f:6]', '[a:1,d:4,e:5]', '[a:1,d:4,f:6]', '[b:2,c:3,e:5]', '[b:2,c:3,f:6]', '[b:2,d:4,e:5]', '[b:2,d:4,f:6]']
+    value = parse_bash_brackets_value(corrupted_input)
+    assert value == [
+       {"a":1,"b":2},
+       {"c":3,"d":4},
+       {"e":5,"f":6}
     ]
