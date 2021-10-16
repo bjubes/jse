b327a9690bc6acf38e654ca5f48e9a806ea81edf
Brian Jubelirer <brian2386@gmail.com>
2021-10-16 16:07:45 -0400

    print version out on success

--- .github/scipts/check-version.sh
+++ .github/scipts/check-version.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+set -e
 EXEC_VER=$(./run.py --version | awk '{print $3'})
 PYPI_VER=$(poetry version | awk '{print $2'})
 
-[[ "$EXEC_VER" == "$PYPI_VER" ]] && echo "PyPi and Executable versions match" && exit 0;
+if [[ "$EXEC_VER" == "$PYPI_VER" ]]; then
+	echo "PyPi and Executable versions match"
+	echo "Version: $PYPI_VER"
+	exit 0
+fi 
+
 echo "Pypi version: $PYPI_VER"
 echo "Executable version: $EXEC_VER"
 echo "Version mismatch"
